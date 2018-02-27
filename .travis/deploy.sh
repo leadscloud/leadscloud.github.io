13f6a4b52b515b112863180618b22d4820ff28f1
Ray Chang <love4026@gmail.com>
2018-02-27 19:14:39 +0800

    modify for travis

---
+++ .travis/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -ev
+export TZ='Asia/Shanghai'
+
+# 先 clone 再 commit，避免直接 force commit
+git clone -b master git@github.com:sbmzhcn/sbmzhcn.github.io.git .deploy_git
+
+cd .deploy_git
+git checkout master
+mv .git/ ../public/
+cd ../public
+
+git add .
+git commit -m "Site updated: `date +"%Y-%m-%d %H:%M:%S"`"
+
+# 同时 push 一份到自己的服务器上
+#git remote add vps git@love4026.org:hexo.git
+
+#git push vps master:master --force --quiet
+git push origin master:master --force --quiet
